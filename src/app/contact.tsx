6c7f3d92234264359430ea4575baedc24d59b15b
Valentin Schnabl <e11848108@student.tuwien.ac.at>
2025-03-09 20:37:31 +0100

    add facebook, enable smooth scroll

--- src/app/contact.tsx
+++ src/app/contact.tsx
@@ -100,6 +100,14 @@ export default function Contact() {
                         </svg>
                         <a href="https://www.instagram.com/edelweissquartett" target="_blank" rel="noopener noreferrer" className="text-blue-600">@edelweissquartett</a>
                     </div>
+
+                     {/* Facebook with icon */}
+                     <div className="flex items-center gap-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                            <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"/>
+                        </svg>
+                        <a href="https://www.facebook.com/profile.php?id=61573836786722" target="_blank" rel="noopener noreferrer" className="text-blue-600">Edelweiß Quartett</a>
+                    </div>
                 </div>
 
                 <div className="hidden md:flex w-[1px] bg-black h-auto self-stretch"></div>
